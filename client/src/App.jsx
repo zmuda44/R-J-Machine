7102366bb1393c76ecb6ba5aaf01483ecfab89ce
Greg Bailey <gregbailey617@gmail.com>
2025-01-18 22:04:11 -0500

    tried link for admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,13 @@ import Header from './components/Header';
 import AdminNavbar from './components/Admin-navbar'
 import Footer from './components/Footer';
 
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
     <p style={{ textAlign: 'center', margin: '10px 0' }}>See below for example of a machine shop webpage. Potential customers can view info about the company and submit for an estimate. 
-    The website also has an admin section. To view admin section, click <a href="/admin">here</a></p>
+    The website also has an admin section. To view admin section, click <Link to="/admin">here</Link></p>
     {window.location.pathname.startsWith('/admin') ? <AdminNavbar /> : <Header /> }
     <Outlet />
     <Footer />
