7e300a38522cc3342a53523d667b831bf80a8f84
Greg Bailey <gregbailey617@gmail.com>
2025-01-18 20:12:58 -0500

    changed require on main.jsx for react router dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom'
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.jsx'
 import './index.css'
 
